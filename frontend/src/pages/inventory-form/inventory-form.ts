d16a952b4bbb12801b8e26830e7b461648b9d5d1
Daniel Limon <cervantesdaniellimon50m@gmail.com>
2026-04-07 13:13:58 -0600

    Estructura de carpetas, componentes, paginas, interfaces y servicios del forntend

---
+++ frontend/src/pages/inventory-form/inventory-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-form',
+  imports: [],
+  templateUrl: './inventory-form.html',
+  styleUrl: './inventory-form.css',
+})
+export class InventoryForm {}
