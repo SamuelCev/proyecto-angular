ce49ee306f36ec2cfc7cc1dedb652bee3421f6e0
Daniel Limon <cervantesdaniellimon50m@gmail.com>
2026-04-09 15:43:44 -0600

    Implementacion de LazyLoading

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,13 @@
 import { Routes } from '@angular/router';
-import { Login } from '../pages/login/login';
-import { Dashboard } from '../pages/dashboard/dashboard';
-import { Inventory } from '../pages/inventory/inventory';
-import { InventoryForm } from '../pages/inventory-form/inventory-form';
-import { Suppliers } from '../pages/suppliers/suppliers';
-import { Movements } from '../pages/movements/movements';
-import { NotFound } from '../pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'inventory', component: Inventory },
-  { path: 'inventory/new', component: InventoryForm },
-  { path: 'inventory/:id/edit', component: InventoryForm },
-  { path: 'suppliers', component: Suppliers },
-  { path: 'movements', component: Movements },
-  { path: '**', component: NotFound },
+  { path: 'login', loadComponent: () => import('../pages/login/login').then(m => m.Login) },
+  { path: 'dashboard', loadComponent: () => import('../pages/dashboard/dashboard').then(m => m.Dashboard) },
+  { path: 'inventory', loadComponent: () => import('../pages/inventory/inventory').then(m => m.Inventory) },
+  { path: 'inventory/new', loadComponent: () => import('../pages/inventory-form/inventory-form').then(m => m.InventoryForm) },
+  { path: 'inventory/:id/edit', loadComponent: () => import('../pages/inventory-form/inventory-form').then(m => m.InventoryForm) },
+  { path: 'suppliers', loadComponent: () => import('../pages/suppliers/suppliers').then(m => m.Suppliers) },
+  { path: 'movements', loadComponent: () => import('../pages/movements/movements').then(m => m.Movements) },
+  { path: '**', loadComponent: () => import('../pages/not-found/not-found').then(m => m.NotFound) },
 ];
