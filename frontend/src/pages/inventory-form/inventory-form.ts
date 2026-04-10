134dd1db6a281072dcd6f9435bac6c51ea85f9b1
Carlos Gonzalez <carlosglezq26@gmail.com>
2026-04-10 14:46:57 -0600

    Implementacion de ambos formularios

--- frontend/src/pages/inventory-form/inventory-form.ts
+++ frontend/src/pages/inventory-form/inventory-form.ts
@@ -1,4 +1,4 @@
-/*import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
