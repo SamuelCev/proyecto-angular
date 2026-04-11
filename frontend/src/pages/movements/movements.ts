19eca5b8afd365fcf65101e5fb450b3cb814d4ff
Daniel Limon <cervantesdaniellimon50m@gmail.com>
2026-04-11 13:31:07 -0600

    Pagina de movimientos implementada

--- frontend/src/pages/movements/movements.ts
+++ frontend/src/pages/movements/movements.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { MovementsService } from '../../service/movements';
+import { Movement } from '@inven-tech/types';
 
 @Component({
   selector: 'app-movements',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './movements.html',
   styleUrl: './movements.css',
 })
-export class Movements {}
+export class Movements implements OnInit {
+  private movementsService = inject(MovementsService);
+  
+  movements = signal<Movement[]>([]);
+
+  ngOnInit(): void {
+    this.movementsService.getAll().subscribe({
+      next: (data) => {
+        this.movements.set(data);
+      },
+      error: (err) => {
+        console.error('Error fetching movements', err);
+      }
+    });
+  }
+}
